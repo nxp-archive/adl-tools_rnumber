2ebf680653b89e1d5bfefaafbe84c5a4f3b144fb
Todd Dukes <tdukes@freescale.com>
2002-07-22 19:40:10 +0000

    cleaned up Makefile. changed simple-number-test.scm and driver.scm to support the interpret/compile methodology.

--- tests/simple-number-test.scm
+++ tests/simple-number-test.scm
@@ -1,10 +1,5 @@
 #!./driver
 
-; (module number-test
-; 	(main main)
-; 	(library bl-rnumber))
-
-
 (define (do-tests a b)
     (print "a=0x" (rnumber-cstr a) " b=0x" (rnumber-cstr b) " rn=?"  (rn=? a b))
     (print "a=0x" (rnumber-cstr a) " b=0x" (rnumber-cstr b) " rn!=?" (rn!=? a b))
@@ -13,21 +8,28 @@
     (print "a=0x" (rnumber-cstr a) " b=0x" (rnumber-cstr b) " rn>?"  (rn>? a b))
     (print "a=0x" (rnumber-cstr a) " b=0x" (rnumber-cstr b) " rn>=?" (rn>=? a b)))
 
-;(define (main argv)
-(print "create from int")
-(let ((a (rnumber-ctor 10))
-      (b (rnumber-ctor 15)))
-  (do-tests a b))
+(define (real-main argv)
+   (print "create from int")
+   (let ((a (rnumber-ctor 10))
+	 (b (rnumber-ctor 15)))
+      (do-tests a b))
 
-(print "create from string")
-(let ((a (rnumber-ctor "10"))
-      (b (rnumber-ctor "15")))
-  (do-tests a b))
+   (print "create from string")
+   (let ((a (rnumber-ctor "10"))
+	 (b (rnumber-ctor "15")))
+      (do-tests a b))
 
-(print "equal numbers")
-(let ((a (rnumber-ctor "10")))
-  (do-tests a a))
+   (print "equal numbers")
+   (let ((a (rnumber-ctor "10")))
+      (do-tests a a))
 
-
-
+   (let ((a (rnumber-ctor "10"))
+	 (b (rnumber-ctor "0xff"))
+	 (c (rnumber-ctor "0xff")))
+      (print "a " a)
+      (print "a " (rnumber-cstr a))
+      (print "b " (rnumber-cstr b))
+      (print "c " (rnumber-cstr c))))
     
+(define (simple-number-test . args)
+   (real-main args))
