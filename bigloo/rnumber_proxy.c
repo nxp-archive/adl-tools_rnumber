0a7620b5fbe682132254211842b79ea03cff2108
Todd Dukes <tdukes@freescale.com>
2002-07-27 03:11:52 +0000

    attempt to add equal functionality

--- bigloo/rnumber_proxy.c
+++ bigloo/rnumber_proxy.c
@@ -15,6 +15,7 @@
 #include "bl-rnumber.h"
 
 extern int GC_finalize_on_demand;
+int rnumber_proxy_is_equal( obj_t lhs, obj_t rhs);
 
 void set_GC_call_finalizers_to_zero()
 {
@@ -27,7 +28,7 @@ static char * rnumber_to_string( RNumber_proxy_t array, char *c, int len ) {
 
 static RNumber_proxy_t rnumber_output( RNumber_proxy_t array, FILE *fout ) {
   char * str = bl_rnumber_cstr(array);
-  fprintf( fout, "%s", str);
+  fprintf( fout, "0x%s", str);
   free (str);
   return array;
 }
@@ -51,6 +52,7 @@ RNumber_proxy_t proxy_from_rnumber( struct RNumber * rnumber)
     array->custom.output     = (hol_t (*)())rnumber_output;
     array->custom.final      = (void (*)(hol_t))rnumber_finalize;
     array->custom.identifier = RNUMBER_IDENTIFIER;
+    array->custom.equal      = (int (*)(hol_t, hol_t))rnumber_proxy_is_equal;
     array->a_t.rnumber = rnumber;
     GC_register_finalizer( array, rnumber_finalize, 0, 0, 0 );
     return HOL_REF( array );
@@ -69,4 +71,27 @@ RNumber_proxy_t proxy_from_rnumber( struct RNumber * rnumber)
 char
 rnumber_proxyp( obj_t o ) {
    return (CUSTOMP( o ) && (!strcmp( CUSTOM( o ).identifier, RNUMBER_IDENTIFIER)));
+}
+
+int rnumber_proxy_is_equal( obj_t lhs, obj_t rhs)
+{
+  int lhs_rnumber = rnumber_proxyp(lhs);
+  int rhs_rnumber = rnumber_proxyp(rhs);
+  int lhs_int = INTEGERP(lhs);
+  int rhs_int = INTEGERP(rhs);
+
+  printf("hello\n");
+  if ( !(lhs_rnumber || lhs_int) || !(rhs_rnumber || rhs_int))
+    return 0;
+  
+  if ( lhs_rnumber ) {
+      if ( rhs_rnumber )
+	return rnumber_rn_equal_rn(RNUM_FROM_PROXY(lhs), RNUM_FROM_PROXY(rhs));
+      else
+	return rnumber_rn_equal_ui(RNUM_FROM_PROXY(lhs), rhs->integer);
+  } 
+  if ( rhs_rnumber )
+    return rnumber_rn_equal_rn(lhs->integer, RNUM_FROM_PROXY(rhs));
+  else
+    return lhs->integer == rhs->integer;
 }
