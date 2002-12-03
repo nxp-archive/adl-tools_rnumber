2b6e1e0c6825ff1ec72bff1d0a165867c122c4af
Todd Dukes <tdukes@freescale.com>
2002-12-03 15:08:04 +0000

    added documentation for rnumber.

--- perl/src/pl_rnumber.c
+++ perl/src/pl_rnumber.c
@@ -46,6 +46,13 @@ XS(_wrap_integerp) {
     cv = cv;
     if ((items < 1) || (items > 1)) 
         croak("Usage: stringp(scalar);");
+
+    printf("svIOK  returns %d\n", SvIOK(ST(0)));
+    printf("svNOK  returns %d\n", SvNOK(ST(0)));
+    printf("svPOK  returns %d\n", SvPOK(ST(0)));
+    printf("svIOKp returns %d\n", SvIOKp(ST(0)));
+    printf("svNOKp returns %d\n", SvNOKp(ST(0)));
+    printf("svPOKp returns %d\n", SvPOKp(ST(0)));
     _result = (SvIOK(ST(0)) != 0);
     ST(argvi) = sv_newmortal();
     sv_setiv(ST(argvi++),(IV) _result);
