e27becf096b70545273ee79b56d532eaeae5ca25
Brian Kahne <bkahne@freescale.com>
2007-02-23 20:13:46 +0000

    I'm adding this file, even though it's generated, because this way the Windows port will work w/o a problem.

---
+++ src/rnumber_feature.h
@@ -0,0 +1,38 @@
+/*
+** ===========================================================================
+** $RCSfile$
+** $Revision: 42 $
+** $Date: 2002-07-15 20:55:48 -0500 (Mon, 15 Jul 2002) $
+** $Author: tdukes $
+** ===========================================================================
+*/
+
+#ifndef __RutilFeature_h__
+#define __RutilFeature_h__ "$Id: rnumber_feature.h.in 42 2002-07-16 01:55:48Z tdukes $"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+
+/* compile time version
+ */
+#define RNUMBER_MAJOR_VERSION				(2)
+#define RNUMBER_MINOR_VERSION				(1)
+#define RNUMBER_MICRO_VERSION				(3)
+#define RNUMBER_BINARY_AGE				(0)
+#define RNUMBER_INTERFACE_AGE				(0)
+#define	RNUMBER_CHECK_VERSION(major,minor,micro)	\
+    (RNUMBER_MAJOR_VERSION > (major) || \
+     (RNUMBER_MAJOR_VERSION == (major) && RNUMBER_MINOR_VERSION > (minor)) || \
+     (RNUMBER_MAJOR_VERSION == (major) && RNUMBER_MINOR_VERSION == (minor) && \
+      RNUMBER_MICRO_VERSION >= (micro)))
+
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+
+#endif
